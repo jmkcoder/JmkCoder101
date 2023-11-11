64448c863c4e3da471541015061b1852db55bc49
JmkCoder <jordan.minkive@outlook.com>
2023-11-11 08:33:15 -0500

    Using Generic Code Sample

---
+++ Components/CodeSample/CodeSampleExample.cs
@@ -0,0 +1,12 @@
+﻿using Components.Component.ViewModels;
+
+namespace Components.CodeSample
+{
+    public partial class CodeSampleViewModel
+    {
+        public override ExampleViewModel Example()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Components/CodeSample/CodeSampleViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Components.Component.ViewModels;
+
+namespace Components.CodeSample
+{
+    public partial class CodeSampleViewModel : ComponentViewModel
+    {
+        public string? Code { get; set; }
+        public string? Language { get; set; }
+        public string? Component { get; set; }
+    }
+}
